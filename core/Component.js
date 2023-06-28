f9318a50fe0d5ea2e7a3b5631674e44297c47949
yjleeinkr <lucy.lee930216@gmail.com>
2023-06-29 01:26:03 +0900

    feat: initial core 컴포넌트 생성

--- core/Component.js
+++ core/Component.js
@@ -1,2 +1,23 @@
-// state, setState, render 를 가진 컴포넌트
-export default class Component {}
+// state, setState, render 를 가진 코어 컴포넌트
+export default class Component {
+  $targetEl;
+  state;
+  constructor($target) {
+    this.$targetEl = $target;
+    this.initiate();
+    this.render();
+  }
+  initiate() {}
+  createTemplate() {
+    return "";
+  }
+  render() {
+    this.$targetEl.innerHTML = this.createTemplate();
+    this.setEvent();
+  }
+  setEvent() {}
+  setState(newState) {
+    this.state = { ...this.state, ...newState };
+    this.render();
+  }
+}
