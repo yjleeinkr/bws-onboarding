8e8bc39dcac5223c7eed5dc2afa16bba0c74a4fb
yjleeinkr <lucy.lee930216@gmail.com>
2023-07-03 01:28:20 +0900

    refac: todo 상태 및 setter 함수들 분리

---
+++ src/state.js
@@ -0,0 +1,39 @@
+import { getLocal, setLocal } from "./utils.js";
+
+export let todos = getLocal("todos") ?? [];
+
+export function setTodos(newTodos) {
+  todos = newTodos;
+  setLocal("todos", todos);
+}
+
+export function addTodo(text) {
+  const id = `${new Date().getTime()}`;
+  const todo = {
+    id,
+    text,
+    isDone: false,
+  };
+  const newTodos = [...todos, todo];
+  setTodos(newTodos);
+  return todo;
+}
+
+export function deleteTodo(id) {
+  const newTodos = todos.filter((todo) => todo.id !== id);
+  setTodos(newTodos);
+}
+
+export function updateTodo(id) {
+  let clickedId;
+  const updatedTodos = todos.map((todo, i) => {
+    if (todo.id === id) {
+      clickedId = i;
+      return { ...todo, isDone: !todo.isDone };
+    } else {
+      return todo;
+    }
+  });
+  setTodos(updatedTodos);
+  return todos[clickedId].isDone;
+}
